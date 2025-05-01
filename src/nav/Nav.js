f984149d3b08fc38086f53e5eade9aa322241a17
gomelkodarya <gomelkodarya@gmail.com>
2025-05-01 10:16:46 +0200

    Fix offset

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -9,19 +9,19 @@ export const Nav = ({isOpen}) => {
             <a href="#skills">Skills</a>
             <a href="#projects">Projects</a>
             <a href="#contact">Contact</a> */}
-            <Link activeClass={style.active} to="main" spy={true} smooth={true} duration={500} offset={-71} onSetActive={(to) => {
+            <Link activeClass={style.active} to="main" spy={true} smooth={true} duration={500} offset={-72} onSetActive={(to) => {
     window.history.replaceState(null, '', `#${to}`);
   }}>Home</Link>
-            <Link activeClass={style.active} to="about-me" spy={true} smooth={true} duration={500} offset={-71} onSetActive={(to) => {
+            <Link activeClass={style.active} to="about-me" spy={true} smooth={true} duration={500} offset={-72} onSetActive={(to) => {
                 window.history.replaceState(null, '', `#${to}`);
             }}>About me</Link>
-            <Link activeClass={style.active} to="skills" spy={true} smooth={true} duration={500} offset={-71} onSetActive={(to) => {
+            <Link activeClass={style.active} to="skills" spy={true} smooth={true} duration={500} offset={-72} onSetActive={(to) => {
     window.history.replaceState(null, '', `#${to}`);
   }}>Skills</Link>
-            <Link activeClass={style.active} to="projects" spy={true} smooth={true} duration={500} offset={-71} onSetActive={(to) => {
+            <Link activeClass={style.active} to="projects" spy={true} smooth={true} duration={500} offset={-72} onSetActive={(to) => {
     window.history.replaceState(null, '', `#${to}`);
   }}>Projects</Link>
-            <Link activeClass={style.active} to="contact" spy={true} smooth={true} duration={500} offset={-71} onSetActive={(to) => {
+            <Link activeClass={style.active} to="contact" spy={true} smooth={true} duration={500} offset={-72} onSetActive={(to) => {
     window.history.replaceState(null, '', `#${to}`);
   }}>Contact</Link>
         </div>
